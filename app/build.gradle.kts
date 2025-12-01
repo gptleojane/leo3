64cd1b185b689f83acfd607344627a78b9278647
LEO <gpt.leojane@gmail.com>
2025-12-01 19:19:43 +0800

    restore minSdk = 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
     id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.leo3"
-    compileSdk = 34
-
+    compileSdk = 36
     buildFeatures {
         viewBinding = true
     }
@@ -15,9 +15,10 @@ android {
     defaultConfig {
         applicationId = "com.example.leo3"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
@@ -31,30 +32,29 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
     kotlinOptions {
         jvmTarget = "11"
     }
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.fragment)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    // 🔥 Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
-    implementation("com.google.firebase:firebase-analytics-ktx")
-    implementation("com.google.firebase:firebase-auth-ktx")
+    // 🔥 Firebase Analytics（必備）
+    implementation("com.google.firebase:firebase-analytics")
 }
